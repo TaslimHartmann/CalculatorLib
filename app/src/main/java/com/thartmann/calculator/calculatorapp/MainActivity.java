dbada10bdf7862a690606748ed0b738d38bc64a9
TaslimHartmann <taslim@cashlez.com>
2016-11-12 12:41:00 +0700

    Add Log

--- app/src/main/java/com/thartmann/calculator/calculatorapp/MainActivity.java
+++ app/src/main/java/com/thartmann/calculator/calculatorapp/MainActivity.java
@@ -2,18 +2,20 @@ package com.thartmann.calculator.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.thartmann.com.calculatorlibrary.Calculator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Calculator calculator = new Calculator();
-        calculator.doAdd(1, 2);
+        Log.d(TAG, "Add: " + calculator.doAdd(1, 2));
     }
 }
